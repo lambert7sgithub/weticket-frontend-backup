b09115cbdb3f71eb95fc36cfe4bbeaabae908865
Lambert <2638498951@qq.com>
2022-08-08 01:20:35 +0800

    feat: clear project

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
-import todoReducer from "../features/todo/todoSlice";
 
 export const store = configureStore({
   reducer: {
-    todoList: todoReducer,
+
   },
 });
--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,21 +1,17 @@
 import React from 'react';
-import { Link, Outlet } from "react-router-dom";
+import {Outlet} from "react-router-dom";
 
 const Layout = () => {
-  const env = process.env;
-  return (
+    const env = process.env;
+    console.log(env);
+    return (
     <div>
-      <h1>Layout's Todo</h1>
-      <p>{JSON.stringify(env)}</p>
-      <nav>
-        <Link to="/">Home</Link> |{" "}
-        <Link to="/about">About</Link>
-      </nav>
-      <div className="content">
-        <Outlet />
-      </div>
+        <div className="content">
+            <Outlet/>
+        </div>
     </div>
-  );
+)
+    ;
 };
 
 export default Layout;
