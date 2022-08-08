73403eda39a7079e875fdfba98793797d92fc482
MATTHEW CHEN (ITA-ISDC-ISD-OOCLL/ZHA) <CHENMA21@OOCL.COM>
2022-08-08 18:58:54 +0800

    fix: fix import

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
 import "antd/dist/antd.min.css";
+import { BrowserRouter } from 'react-router-dom';
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
