a9ff7e3646305529d5282f7d2c1fb19ec1d58d10
FUFI2 <1447503159@qq.com>
2022-08-09 10:51:38 +0800

    feat MovieItem to function

--- src/feature/MovieShow/MovieItem.jsx
+++ src/feature/MovieShow/MovieItem.jsx
@@ -3,25 +3,6 @@ import "./movieitem.css";
 import { Rate } from "antd";
 import { Image } from "antd";
 
-// export default class MovieItem extends React.Component {
-   
-//   constructor(props) {
-//     super(props);
-//     this.state = {};
-//   }
-
-//   render() {
-
-//     return (
-//       <div className="box">
-//         <Image width={150} height={200} src={this.props.picture} />
-//         <h4 className="moviename">{this.props.movie_name}</h4>
-//         <h5>评分：{this.props.score}</h5>
-//         <Rate allowHalf defaultValue={4.5} />
-//       </div>
-//     );
-//   }
-// }
 
 function MovieItem(props){
  
@@ -36,4 +17,5 @@ function MovieItem(props){
   }
 
   export default MovieItem;
+
 
