03e18565015591b783cdc4070bd027bf2b7d472a
MATTHEW CHEN (ITA-ISDC-ISD-OOCLL/ZHA) <CHENMA21@OOCL.COM>
2022-08-11 00:16:13 +0800

    refactor: seat and movie css

--- src/feature/Seat/Seat.jsx
+++ src/feature/Seat/Seat.jsx
@@ -1,57 +1,65 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./Seat.css";
 import { nanoid } from "nanoid";
 import MovieDetail from "./MovieDetail";
-import {getSeatDetail} from "../../api/movie";
-import {message} from "antd";
+import { getSeatDetail } from "../../api/movie";
+import { message } from "antd";
 
 export default function Seat(props) {
   // id, status,
   //false 已选 true 可选 2 已选中
 
-  const [seats, setSeats] = useState(Array.from({length: 10}, () => Array.from({length: 12}, () => ({
-    id: nanoid(),
-    isSold: false,
-  }))))
+  const [seats, setSeats] = useState(
+    Array.from({ length: 10 }, () =>
+      Array.from({ length: 12 }, () => ({
+        id: nanoid(),
+        isSold: false,
+      }))
+    )
+  );
 
-  const movieDetail = getSeatDetail(props.movieId, props.cinemaId, props.scnId).then(res => {
-    return res.data;
-  }).catch(() => {
-    message.error("无法拉取电影详情");
-  });
+  const movieDetail = getSeatDetail(props.movieId, props.cinemaId, props.scnId)
+    .then((res) => {
+      return res.data;
+    })
+    .catch(() => {
+      message.error("无法拉取电影详情");
+    });
   const changeSeatStatus = (rowIndex, colIndex) => {
     let copy = [...seats];
     copy[rowIndex][colIndex].isSold = !copy[rowIndex][colIndex].isSold;
     setSeats(copy);
-  }
+  };
 
   return (
-      <div className="seat-page">
-        <div className="seats-block">
-          <div className="seats-container">
-            <div className="screen-container" style={{left: "5px"}}>
-              <div className="screen">银幕中央</div>
-              <div className="c-screen-line"></div>
-            </div>
+    <div className="seats-content">
+      <div className="seats-block">
+        <div className="screen-container" style={{ left: "5px" }}>
+          <div className="screen">银幕中央</div>
+        </div>
 
-            <div className="seats-wrapper">
-              <div style={{padding: "0 40px", width: "580px"}}>
-                {seats.map((items, rowIndex) =>
-                    items.map((item, colIndex) => (
-                        <span key={colIndex}>
-                    <span className={item.isSold === false ? "seat selectable item" : "seat sold item"}
-                          onClick={() => changeSeatStatus(rowIndex, colIndex)}/>
-                  </span>
-                    ))
-                )}
-              </div>
-            </div>
+        <div className="seats-wrapper">
+          <div style={{ padding: "0 40px", width: "580px" }}>
+            {seats.map((items, rowIndex) =>
+              items.map((item, colIndex) => (
+                <span key={colIndex} style={{paddingBottom:'20px'}}>
+                  <span
+                    className={
+                      item.isSold === false
+                        ? "seat selectable item"
+                        : "seat sold item"
+                    }
+                    onClick={() => changeSeatStatus(rowIndex, colIndex)}
+                  />
+                </span>
+              ))
+            )}
           </div>
         </div>
-        <div className="seat-movie-detail">
-          <MovieDetail movieDetail={movieDetail}/>
-        </div>
       </div>
+      <div>
+        <MovieDetail movieDetail={movieDetail} />
+      </div>
+    </div>
   );
 }
-
