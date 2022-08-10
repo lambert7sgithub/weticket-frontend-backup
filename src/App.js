7e11ef052dab075a1b28a039e70f9b7aa830159e
Lambert <lambert.lin@oocl.com>
2022-08-10 17:06:25 +0800

    fix: replace a link with router link

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import WeTicketLayout from "./layout/WeTicketLayout";
-import { Route, Routes } from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 import React from "react";
+import "./App.less";
 import Home from "./feature/home/Home";
 import NotFoundPage from "./pages/NotFoundPage";
 import MovieDetails from "./feature/MovieShow/MovieDetails";
@@ -10,31 +11,31 @@ import Friend from "./pages/Friend";
 import Login from "./Login/Login";
 import Register from "./Register/Register.jsx";
 import Pay from "./feature/Pay/Pay";
-import Schduling from "./feature/Scheduling/Scheduling";
 import Seat from "./feature/Seat/Seat";
-import "./App.less";
+import Scheduling from "./feature/Scheduling/Scheduling";
 
 function App() {
-  return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<WeTicketLayout />}>
-          <Route index element={<Home />} />
-          <Route path="movie" element={<MovieShow />} />
-          <Route path="food" element={<Food />} />
-          <Route path="friend" element={<Friend />} />
-          <Route path="*" element={<NotFoundPage />} />
-          <Route path="login" element={<Login />} />
-          <Route path="register" element={<Register />} />
-          <Route path="MovieDetails/:movieId" element={<MovieDetails />} />
-          <Route path="Pay" element={<Pay />} />
-          <Route path="Schduling/:movieId" element={<Schduling />} />
-          <Route path="Scheduling/:movieId/screening/:screeningId/Seat" element={<Seat />} />
-        </Route>
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Routes>
+                <Route path="/" element={<WeTicketLayout/>}>
+                    <Route index element={<Home/>}/>
+                    <Route path="movie" element={<MovieShow/>}/>
+                    <Route path="food" element={<Food/>}/>
+                    <Route path="friend" element={<Friend/>}/>
+                    <Route path="login" element={<Login/>}/>
+                    <Route path="register" element={<Register/>}/>
+                    <Route path="MovieDetails/:movieId" element={<MovieDetails/>}/>
+                    <Route path="Pay" element={<Pay/>}/>
+                    <Route path="Scheduling/:movieId/screening/:screeningId/Seat" element={<Seat/>}/>
+                    <Route path="Scheduling/:movieId" element={<Scheduling/>}/>
+
+                    <Route path="*" element={<NotFoundPage/>}/>
+                </Route>
+                <Route path="*" element={<NotFoundPage/>}/>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
--- src/feature/MovieShow/MovieDetails.jsx
+++ src/feature/MovieShow/MovieDetails.jsx
@@ -1,18 +1,15 @@
-import React, { useState, useEffect } from 'react';
-import { Layout } from 'antd';
-import { Divider } from 'antd';
-import { Card } from 'antd';
+import React, {useEffect, useState} from 'react';
+import {Card, Divider, Layout} from 'antd';
 import "./CSS/MovieDetails.css";
-import { RightCircleOutlined, SearchOutlined } from '@ant-design/icons';
-import { getMovieById } from "../../api/movie";
-import { useParams } from 'react-router-dom';
+import {RightCircleOutlined, SearchOutlined} from '@ant-design/icons';
+import {getMovieById} from "../../api/movie";
+import {Link, useParams} from 'react-router-dom';
 
 
 const MovieDetails = () => {
 
-    let { movieId } = useParams();
-    const {Content } = Layout;
-    // let {ScreenUrl} = "Schduling/"+{movieId};
+    let {movieId} = useParams();
+    const {Content} = Layout;
 
     const [movie, setMovie] = useState({});
 
@@ -27,71 +24,75 @@ const MovieDetails = () => {
     console.log(movie);
 
 
-
     return (
         <Layout>
-            <Layout style={{ display: "flex" }}>
+            <Layout style={{display: "flex"}}>
                 <Content>
-                    <div style={{ display: "flex" }}>
-                        <div style={{ display: "flex" }}>
-                            <img src={movie.picture} alt="" style={{ margin: "120px 50px 200px 50px", height: "600px" }}></img>
-                            <div style={{ marginRight: "10px", width: "700px" }}>
-                                <div style={{ height: "400px", marginTop: "100px", fontSize: "30px" }}>
-                                    <Card style={{ width: "600px", fontSize: "20px" }}>
+                    <div style={{display: "flex"}}>
+                        <div style={{display: "flex"}}>
+                            <img src={movie.picture} alt=""
+                                 style={{margin: "120px 50px 200px 50px", height: "600px"}}></img>
+                            <div style={{marginRight: "10px", width: "700px"}}>
+                                <div style={{height: "400px", marginTop: "100px", fontSize: "30px"}}>
+                                    <Card style={{width: "600px", fontSize: "20px"}}>
                                         <p>
-                                            <span style={{ fontSize: "40px" }}><RightCircleOutlined /> {movie.movieName}</span>
+                                            <span
+                                                style={{fontSize: "40px"}}><RightCircleOutlined/> {movie.movieName}</span>
                                         </p>
-                                        <Divider />
+                                        <Divider/>
                                         <p>
                                             <span>导演：</span>
                                             {movie.actor}
-                                    </p>
+                                        </p>
                                         <p>
                                             <span>主演：</span>
                                             {movie.directedBy}
-                                </p>
+                                        </p>
                                         <p>
                                             <span>语言：</span>
                                             {movie.language}
-                                </p>
+                                        </p>
                                         <p>
                                             <span>片长：</span>
-                                    {movie.length}
-                                </p>
+                                            {movie.length}
+                                        </p>
                                         <p>
                                             <span>上映时间：</span>
                                             {movie.length}
-                                </p>
+                                        </p>
 
 
                                     </Card>
                                 </div>
 
                                 <div>
-                                    <Card style={{ width: "600px", fontSize: "25px" }}>
+                                    <Card style={{width: "600px", fontSize: "25px"}}>
                                         <p>
 
-                                            <span style={{ fontSize: "25px" }}> <SearchOutlined /> 剧情描述:</span>
+                                            <span style={{fontSize: "25px"}}> <SearchOutlined/> 剧情描述:</span>
                                         </p>
-                                        <Divider />
-                                        <div style={{ fontSize: "15px" }}>
-                                            <p>　 {movie.info}</p>
+                                        <Divider/>
+                                        <div style={{fontSize: "15px"}}>
+                                            <p>  {movie.info}</p>
                                         </div>
                                     </Card>
                                 </div>
                             </div>
                         </div>
-                        <div style={{ alignItems: "center" }}>
-                            <div style={{ marginTop: "280px", alignItems: "center" }}>
+                        <div style={{alignItems: "center"}}>
+                            <div style={{marginTop: "280px", alignItems: "center"}}>
                                 <Card>
-                                    <a className="push_button red" href={'/Schduling/'+movieId}>Buy Ticket</a>
-           
+                                    <Link to={"/Scheduling/" + movieId} className="push_button red">
+                                        Buy Ticket
+                                    </Link>
                                 </Card>
                             </div>
-                            <br />
+                            <br/>
                             <div>
                                 <Card>
-                                    <a className="push_button blue" href="/">Buy Food</a>
+                                    <Link to={"/"} className="push_button blue">
+                                        Buy Ticket
+                                    </Link>
                                 </Card>
                             </div>
                         </div>
