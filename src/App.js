1728356ad66477668c945f1b08b45d8091a416eb
Elodiiie <983793727@qq.com>
2022-08-10 08:53:40 +0800

    fix: update schedule search UI warning

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import WeTicketLayout from "./layout/WeTicketLayout";
-import {Route, Routes} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import React from "react";
 import Home from "./feature/home/Home";
 import NotFoundPage from "./pages/NotFoundPage";
@@ -8,32 +8,31 @@ import MovieShow from "./feature/MovieShow/MovieShow";
 import Food from "./pages/Food";
 import Friend from "./pages/Friend";
 import Login from "./Login/Login";
-import Register from './Register/Register.jsx';
-import Pay from './feature/Pay/Pay';
+import Register from "./Register/Register.jsx";
+import Pay from "./feature/Pay/Pay";
 import "./App.css";
 import ScheduleTable from "./feature/Scheduling/Scheduling";
 
 function App() {
-    return (
-        <div className="App">
-            <Routes>
-                <Route path="/" element={<WeTicketLayout />}>
-                    <Route index element={<Home />} />
-                    <Route path='movie' element={<MovieShow />} />
-                    <Route path='food' element={<Food />} />
-                    <Route path='friend' element={<Friend />} />
-                    <Route path="*" element={<NotFoundPage />} />
-                    <Route path="login" element={<Login />} />
-                    <Route path="register" element={<Register />} />
-                    <Route path="MovieDetials/:movieId" element={<MovieDetails />} />
-                    <Route path="Pay" element={<Pay />} />
-                </Route>
-                <Route path="*" element={<NotFoundPage />} />
-                <Route path="s" element={<ScheduleTable/>} />
-
-            </Routes>
-        </div>
-    );
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<WeTicketLayout />}>
+          <Route index element={<Home />} />
+          <Route path="movie" element={<MovieShow />} />
+          <Route path="food" element={<Food />} />
+          <Route path="friend" element={<Friend />} />
+          <Route path="*" element={<NotFoundPage />} />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+          <Route path="MovieDetials/:movieId" element={<MovieDetails />} />
+          <Route path="Pay" element={<Pay />} />
+          <Route path="s" element={<ScheduleTable />} />
+        </Route>
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
