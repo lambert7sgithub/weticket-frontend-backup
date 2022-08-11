786152f6b1b0a5e4c1b192f5c3b5b2ab4f495510
FUFI2 <1447503159@qq.com>
2022-08-11 13:44:27 +0800

    feat: changes orderlist.css

--- src/feature/personalCenter/OrderList.jsx
+++ src/feature/personalCenter/OrderList.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { ShoppingCartOutlined } from "@ant-design/icons";
 import "./orders/order.css";
 const OrderList = (props) => {
   return (
@@ -23,35 +22,3 @@ const OrderList = (props) => {
 };
 
 export default OrderList;
-
-{
-  /* <table class="listtable">
-        <tr>
-          <th>
-            <ShoppingCartOutlined
-              style={{
-                fontSize: "20px",
-                color: "red",
-              }}
-            />
-            &nbsp;订单号:sdsdfasdasd2555'
-          </th>
-        </tr>
-        <tr>
-          <td>
-            <div className="orderShow">
-              <img alt="" src='/photos/1.jpg'></img>
-              <div className="showRight">
-                <ul>
-                  <li>《寻梦环游记》</li>
-                  <li>《珠海巨无霸影城》</li>
-                  <li> 《周五1月1日 13:00》</li>
-                </ul>
-              </div>
-            </div>
-          </td>
-          <td className="monyeStyle">￥200</td>
-          
-        </tr>
-      </table> */
-}
