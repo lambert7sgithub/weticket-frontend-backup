a75eebef5a578964d633c97c753af02fac241689
FUFI2 <1447503159@qq.com>
2022-08-11 13:33:22 +0800

    feat: finish the code of orderlist in perdonOrder

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import WeTicketLayout from "./layout/WeTicketLayout";
-import { Route, Routes } from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 import React from "react";
+import "./App.less";
 import Home from "./feature/home/Home";
 import NotFoundPage from "./pages/NotFoundPage";
 import MovieDetails from "./feature/MovieShow/MovieDetails";
 import MovieShow from "./feature/MovieShow/MovieShow";
-import Food from "./pages/Food";
+import Food from "./feature/Food/Food";
 import Friend from "./pages/Friend";
-import Login from "./Login/Login";
-import Register from "./Register/Register.jsx";
+import Login from "./feature/Login/Login";
+import Register from "./feature/Register/Register.jsx";
 import Pay from "./feature/Pay/Pay";
-import "./App.css";
-import ScheduleTable from "./feature/Scheduling/Scheduling";
-import Order from "./feature/orders/order"; 
+import Seat from "./feature/Seat/Seat";
+import Scheduling from "./feature/Scheduling/Scheduling";
+import Order from "./feature/orders/Order"; 
 import PersonOrder from "./feature/personalCenter/PersonOrder";
+
 
 function App() {
   return (
@@ -24,17 +26,21 @@ function App() {
           <Route path="movie" element={<MovieShow />} />
           <Route path="food" element={<Food />} />
           <Route path="friend" element={<Friend />} />
-          <Route path="*" element={<NotFoundPage />} />
           <Route path="login" element={<Login />} />
           <Route path="register" element={<Register />} />
-          <Route path="MovieDetials/:movieId" element={<MovieDetails />} />
+          <Route path="MovieDetails/:movieId" element={<MovieDetails />} />
           <Route path="Pay" element={<Pay />} />
-          <Route path="s" element={<ScheduleTable />} />
+          <Route path="Scheduling/:movieId" element={<Scheduling />} />
+          <Route path="Scheduling/:movieId/screening/:screeningId/seat" element={<Seat />} />
           <Route path="order" element={<Order />}/>
           <Route path="personorder" element={<PersonOrder />}/>
+          <Route path="*" element={<NotFoundPage />} />
         </Route>
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </div>
   );
 }
+
+export default App;
+
